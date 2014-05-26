ac6f939bb9a50b96eeecfad81f22da85ddc1a1db
chang47 <coralbue@gmail.com>
2014-05-26 01:48:27 -0700

    Added feature
    Can make comma list into a list

--- client/spintax.js
+++ client/spintax.js
@@ -3,19 +3,32 @@ Template.convert.events({
   'click input#button' : function () {
     var words = document.getElementById("convert").value;
     var spintax = "{";
-    if(document.getElementById("list").checked) {
-      var array = words.split('\n');
-    } else if (document.getElementById("comma").checked) {
+    if(document.getElementById("commatolist").checked) {
+      var list = "";
       var array = words.split(', ');
       if(array.length < 2) {
         var array = words.split(",");
       }
+      list += array[0]; 
+      for(var i = 1; i < array.length; i++) {
+        list += "\n" + array[i];
+      }
+      document.getElementById("result").value = list;
+    } else {
+      if(document.getElementById("list").checked) {
+        var array = words.split('\n');
+      } else if (document.getElementById("comma").checked) {
+        var array = words.split(', ');
+        if(array.length < 2) {
+          var array = words.split(",");
+        }
+      }
+      spintax += array[0];
+      for(var i = 1; i < array.length; i++) {
+          spintax += "|" + array[i];
+      }
+      spintax += "}";
+      document.getElementById("result").value = spintax;
     }
-    spintax += array[0];
-    for(var i = 1; i < array.length; i++) {
-        spintax += "|" + array[i];
-    }
-    spintax += "}";
-    document.getElementById("result").value = spintax;
   }
 });
